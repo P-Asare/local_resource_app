3d222434d2aff4523d9d4145c3e6c39391fe7097
P-Asare <pinopalal11@gmail.com>
2024-06-04 10:13:10 +0000

    correct material app problem

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const LoginScreen();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
   }
 }
